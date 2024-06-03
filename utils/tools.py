e675d1f67827599b0eef7f28fd3c13e2c8dedcc8
DengXL <dengxl0520@gmail.com>
2024-06-03 21:01:57 +0800

    [update] tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -65,3 +65,62 @@ def hausdorff_distance(mask1: Tensor, mask2: Tensor, percentile: int = 95):
     hausdorff_dist = np.percentile(dist, percentile)
 
     return hausdorff_dist
+
+
+def corr(x, y):
+    '''
+        x : gt
+        y : pred
+        A = mean( (y_real - mean(y_real)) * (y_predict - mean(y_predict)) )
+        B = std(y_real) * std(y_predict)
+        corr = A / B
+    '''
+    A = ((x - x.mean()) * (y - y.mean())).mean()
+    B = std(x) * std(y)
+    corr = A / B
+    return corr
+
+def bias(x, y):
+    '''
+        x : gt
+        y : pred
+        bias = sum( abs(y_real - y_predict) ) / len( y_real )
+    '''
+    return (abs(x - y)).mean()
+
+def std(x):
+    '''
+        A = (y - mean(y)) * (y - mean(y))
+        std = sqrt( sum(A) / n )
+    '''
+    # A = ((x - x.mean()) * (x - x.mean())).mean()
+    # std = np.sqrt(A)
+    return np.std(x)
+
+def draw_sem_seg_by_cv2_sum(image, gt_sem_seg, pred_sem_seg, palette):
+    '''
+        image: [3,h,w] numpy.ndarray
+        gt_sem_seg: [h,w] numpy.ndarray
+        pred_sem_seg: [h,w] numpy.ndarray
+        palette: [bg, gt, pred, overlap] numpy.ndarray
+    '''
+    gt_sem_seg = gt_sem_seg.astype(np.uint8)
+    pred_sem_seg = pred_sem_seg.astype(np.uint8)
+    mask = 2 * pred_sem_seg + gt_sem_seg
+    mask = mask.squeeze()
+    
+    ids = np.unique(mask)
+    color_mask = np.zeros_like(image)
+    for idx in ids:
+        color_mask[0][mask == idx] = palette[idx][0]
+        color_mask[1][mask == idx] = palette[idx][1]
+        color_mask[2][mask == idx] = palette[idx][2]
+    
+    results = cv2.addWeighted(image, 0.2, color_mask, 0.8, 0)
+    # for i in range(mask.shape[0]):
+    #     for j in range(mask.shape[1]):
+    #         if mask[i,j] !=0:
+    #             image[...,i,j] = results[...,i,j]
+    mask = np.expand_dims(mask, 0).repeat(3,axis=0) # np
+    image[mask != 0] = results[mask != 0]
+    return image
